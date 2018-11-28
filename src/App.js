0ce369eae4d27d23dada7dea2c95a23a24f1720e
Drian Hillman <dhillman24@gmail.com>
2018-11-27 22:29:50 -0500

    Adds initial state to the component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: false }
+      ]
+    };
+  }
   render() {
     return (
       <div className='App'>
-        <ToDo />
-        <ToDo />
+        {this.state.todos.map((todo, index) => (
+          <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted} />
+        ))}
       </div>
     );
   }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,7 +4,10 @@ export default class ToDo extends Component {
   render() {
     return (
       <>
-        <li>A todo will go here.</li>
+        <li>
+          <input type='checkbox' checked={this.props.isCompleted} />
+          <span>{this.props.description}</span>
+        </li>
       </>
     );
   }
