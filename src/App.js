924a194d23b4899ef745333731eb5d5d6e86abf1
Drian Hillman <dhillman24@gmail.com>
2018-11-27 23:10:18 -0500

    Adds event handling & functionality to the ToDo list

--- src/App.js
+++ src/App.js
@@ -11,15 +11,47 @@ class App extends Component {
         { description: 'Walk the cat', isCompleted: true },
         { description: 'Throw the dishes away', isCompleted: false },
         { description: 'Buy new dishes', isCompleted: false }
-      ]
+      ],
+      newTodoDescription: ''
     };
   }
+
+  toggleComplete(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.newTodoDescription) {
+      const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+      this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ newTodoDescription: e.target.value });
+  }
+
   render() {
     return (
       <div className='App'>
-        {this.state.todos.map((todo, index) => (
-          <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted} />
-        ))}
+        <ul>
+          {this.state.todos.map((todo, index) => (
+            <ToDo
+              key={index}
+              description={todo.description}
+              isCompleted={todo.isCompleted}
+              toggleComplete={() => this.toggleComplete(index)}
+            />
+          ))}
+        </ul>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input type='text' value={this.state.newTodoDescription} onChange={e => this.handleChange(e)} />
+          <input type='submit' />
+        </form>
       </div>
     );
   }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,7 +5,7 @@ export default class ToDo extends Component {
     return (
       <>
         <li>
-          <input type='checkbox' checked={this.props.isCompleted} />
+          <input type='checkbox' checked={this.props.isCompleted} onChange={this.props.toggleComplete} />
           <span>{this.props.description}</span>
         </li>
       </>
