62fc79fd3c62f4c13292fa049442635005de4ebc
Drian Hillman <dhillman24@gmail.com>
2018-11-27 22:08:42 -0500

    Adds li ToDo component | Removes some CRA boilerplate

---
+++ src/components/ToDo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ToDo extends Component {
+  render() {
+    return (
+      <>
+        <li>A todo will go here.</li>
+      </>
+    );
+  }
+}
